cc81ccca00f0cffba6b4e0781006c04a2d0f76f5
Yunho Shin <yunho1025@gmail.com>
2026-05-17 22:49:01 +1000

    Initialize React app with WorkoutTracker component

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WorkoutTracker from './forge-workout';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<WorkoutTracker />);
